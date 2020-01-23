d07e1b2d2ba592a57d58bb6eda9b6f00c61e4195
VMware Code <noreply@vmware.com>
2020-01-23 02:23:22 +0000

    Initial commit by wroever

--- link_quality_events_example.py
+++ link_quality_events_example.py
@@ -1,6 +1,6 @@
 #
 # Fetch link quality events for an Edge (consistent with Orchestrator UI usage)
-# 
+#
 # Usage: VC_USERNAME='user@velocloud.net' VC_PASSWORD=s3cret python link_quality_events_example.py
 #
 
@@ -9,29 +9,33 @@ from datetime import datetime, timedelta
 from client import *
 
 # EDIT THESE
-VCO_HOSTNAME = 'vcoX-usca1.velocloud.net'
+VCO_HOSTNAME = 'vcoX.velocloud.net'
 ENTERPRISE_ID = 1
 EDGE_ID = 1
 INTERVAL_START = datetime.now() - timedelta(hours=12)
-NUM_SAMPLES = 133
+NUM_SAMPLES = 50 # Default value enforced by the API
+IS_OPERATOR = False
+
+def datetime_to_epoch_ms(dtm):
+    return int(dtm.timestamp()) * 1000
 
 def main():
 
     client = VcoRequestManager(VCO_HOSTNAME)
-    client.authenticate(os.environ['VC_USERNAME'], os.environ['VC_PASSWORD'], is_operator=os.environ.get('VC_OPERATOR', False))
+    client.authenticate(os.environ['VC_USERNAME'], os.environ['VC_PASSWORD'], is_operator=os.environ.get('VC_OPERATOR', IS_OPERATOR))
 
     result = client.call_api('linkQualityEvent/getLinkQualityEvents', {
         "enterpriseId": ENTERPRISE_ID,
         "edgeId": EDGE_ID,
         "interval": {
-            "start": INTERVAL_START.isoformat()
-        },
-        "maxSamples": NUM_SAMPLES
+            "start": datetime_to_epoch_ms(INTERVAL_START)
+        }
+        # "maxSamples": NUM_SAMPLES
     })
 
     # `result` is a map where each key is a link `internalId`, values contain link-specific summary
     # QoE scores and a time series of event samples
-    print result
+    print(result)
 
 
 if __name__ == '__main__':
